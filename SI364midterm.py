3d5990fed10871e6862fffe32538f2cf94f69921
Steph Schouman <svschou@umich.edu>
2018-03-08 20:53:17 -0500

    Added generic outlines for models, forms and app.configs

--- SI364midterm.py
+++ SI364midterm.py
@@ -6,8 +6,9 @@
 # Import statements
 import os
 from flask import Flask, render_template, session, redirect, url_for, flash, request
+from flask_script import Manager, Shell
 from flask_wtf import FlaskForm
-from wtforms import StringField # Note that you may need to import more here! Check out examples that do what you want to figure out what.
+from wtforms import StringField, SubmitField, RadioField# Note that you may need to import more here! Check out examples that do what you want to figure out what.
 from wtforms.validators import Required # Here, too
 from flask_sqlalchemy import SQLAlchemy
 
@@ -17,9 +18,15 @@ app.debug = True
 app.use_reloader = True
 
 ## All app.config values
+app.config['SECRET_KEY'] = 'hardtoguessstringfromsi364thisisnotsupersecurebutitsok'
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/si364midterm_test"
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 ## Statements for db setup (and manager setup if using Manager)
+manager = Manager(app)
+
 db = SQLAlchemy(app)
 
 
@@ -42,6 +49,17 @@ class Name(db.Model):
     def __repr__(self):
         return "{} (ID: {})".format(self.name, self.id)
 
+# class HogwartsStudents(db.Model):
+#     pass
+
+# class HogwartsHouses(db.Model):
+#     pass
+
+# class ImaginaryStudents(db.Model):
+#     pass
+
+# class ImaginaryHouses(db.Model):
+#     pass
 
 
 ###################
@@ -52,6 +70,12 @@ class NameForm(FlaskForm):
     name = StringField("Please enter your name.",validators=[Required()])
     submit = SubmitField()
 
+class HogwartsStudentForm(FlaskForm):
+    name = StringField("Enter the name of a Hogwarts Student: ",validators=[Required()])
+    pass
+
+# class ImaginaryStudentForm(FlaskForm):
+#     pass
 
 
 #######################
@@ -74,12 +98,17 @@ def all_names():
     names = Name.query.all()
     return render_template('name_example.html',names=names)
 
-
+@app.route('/hogwarts')
+def hogwarts():
+    return render_template('home.html')
 
 
 
 
 ## Code to run the application...
+if __name__ == '__main__':
+    db.create_all()
+    manager.run()
 
 # Put the code to do so here!
 # NOTE: Make sure you include the code you need to initialize the database structure when you run the application!
